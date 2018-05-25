1faa79b3ef1ac9eb61daa5733808740a137329ff
yokoyama-haruki <yokoyama.haruki@jp.fujitsu.com>
2018-05-25 09:41:40 +0900

    impl

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Control.Lens (to, only, (^?), ix, toListOf)
+import Data.ByteString.Lazy (ByteString)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding.Error (lenientDecode)
+import Data.Text.Lazy.Encoding (decodeUtf8With)
+import Network.HTTP.Client (Response)
+import Network.Wreq (responseBody, get)
+import Text.Taggy (Node)
+import Text.Taggy.Lens (html, elements, children, contents,allNamed)
+
+texts :: Response ByteString -> [Text]
+texts = toListOf
+  $ responseBody . to (decodeUtf8With lenientDecode)
+  . html . allNamed (only "tr") . children
+  . to (toListOf $ ix 0 . contents)
+  . to T.concat
+
+main :: IO ()
+main = get "https://matonix.github.io/taggy-lens-sample/sample.html"
+  >>= print <$> texts
